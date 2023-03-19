ee0b1426c5e993b581016180281c9edc8d732dc8
unknown <shimantodevjs@gmail.com>
2023-03-19 10:16:30 +0300

    final commit

--- src/components/jokes.js
+++ src/components/jokes.js
@@ -2,8 +2,16 @@ function Jokes(props){
 
      return(
         <div className="jokes-card">
-            <p className="joke">{props.joke}</p>
-            <p className="punchline">Punchline: {props.punchline}</p>
+            {props.joke && <p className="jokes-items">Joke: {props.joke}</p>}
+            {props.punchline &&<p className="jokes-items">Punchline: {props.punchline}</p>}
+            <div className="jokes-items reactions">
+            {props.upVotes && <p><i class="fa-regular fa-thumbs-up"></i> {props.upVotes}</p>}
+            {props.downVotes && <p><i class="fa-regular fa-thumbs-down"></i> {props.downVotes}</p>}
+            </div>
+            <div className="jokes-items comments">
+             { props.comments && <img src={props.propic} alt="" />}
+              {props.comments &&  <p>{props.comments}</p>}
+            </div>
         </div>
      )
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Jokes from './components/jokes';
+import img from './images/propic.png'
 
 function App(){
     return(
@@ -9,22 +10,31 @@ function App(){
             <Jokes
             joke="1. My wife told me to stop acting like a flamingo."
             punchline="I had to put my foot down."
+            upVotes={1}
+            propic={img}
+            comments= {["what a bad joke"]}
             />
             <Jokes
             joke="2. What is Whitney Houston's favorite type of coordination? "
             punchline="HAAANNNNND EYEEEEEEE."
+            upVotes={1}
+            downVotes={6}
             />
             <Jokes
             joke="3. Why are gay people always smiling?"
             punchline="Because they can't keep a straight face."
+             upVotes={100112}
+             downVotes={990}
             />
             <Jokes
             joke="4. Why did David Hasselhoff change his name to “The Hoff?”"
             punchline="It's less hassle."
+            downVotes={9}
             />
             <Jokes
             joke="5. Why does Waldo wear stripes?"
             punchline="Because he doesn't want to be spotted."
+            upVotes={115}
             />
         </div>
     )
