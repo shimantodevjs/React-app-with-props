6b097e31542572a8c1727ed624faea5cfacf204f
unknown <shimantodevjs@gmail.com>
2023-03-20 21:07:55 +0300

    final commit

---
+++ src/jokesdata.js
@@ -0,0 +1,26 @@
+export default [
+    {
+        joke:"1. My wife told me to stop acting like a flamingo.",
+        punchline:"I had to put my foot down."
+    },
+
+    {
+        joke:"2. What is Whitney Houston's favorite type of coordination? ",
+        punchline:"HAAANNNNND EYEEEEEEE."
+    },
+
+    {
+        joke:"3. Why are gay people always smiling?",
+        punchline:"Because they can't keep a straight face."
+    },
+
+    {
+        joke:"4. Why did David Hasselhoff change his name to “The Hoff?”",
+        punchline:"It's less hassle."
+    },
+
+    {
+        joke:"5. Why does Waldo wear stripes?",
+        punchline:"Because he doesn't want to be spotted."
+    }
+]
